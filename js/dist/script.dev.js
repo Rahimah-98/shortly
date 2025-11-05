b96bc01c165b4ba7cddda408735e790064dbd2f8
Rahimah Ansari <rahimaansari98@gmail.com>
2025-11-05 08:04:02 +0430

    Implemented the Mobile Menu

--- js/dist/script.dev.js
+++ js/dist/script.dev.js
@@ -1,12 +1,24 @@
 "use strict";
 
+var btn = document.getElementById('menu-btn');
+var menu = document.getElementById('menu');
 var input = document.getElementById('link-input');
 var linkForm = document.getElementById('link-form');
-var errMsg = document.getElementById('err-msg'); // Validate a URL
+var errMsg = document.getElementById('err-msg');
+btn.addEventListener('click', navToggle);
+linkForm.addEventListener('submit', formSubmit); // Toggle Mobile Menu
+
+function navToggle() {
+  btn.classList.toggle('open');
+  menu.classList.toggle('flex');
+  menu.classList.toggle('hidden');
+} // Validate a URL
+
 
 function validURL(str) {
   var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
   '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + '((\\d{1,3}\\.){3}\\d{1,3}))' + '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + '(\\?[;&a-z\\d%_.~+=-]*)?' + '(\\#[-a-z\\d_]*)?$', 'i');
+  return !!pattern.test(str);
 }
 
 function formSubmit(e) {
--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+
 const input = document.getElementById('link-input');
 const linkForm = document.getElementById('link-form');
 const errMsg = document.getElementById('err-msg');
+
+btn.addEventListener('click', navToggle);
+linkForm.addEventListener('submit', formSubmit);
+
+// Toggle Mobile Menu
+function navToggle() {
+  btn.classList.toggle('open');
+  menu.classList.toggle('flex');
+  menu.classList.toggle('hidden');
+}
 
 // Validate a URL
 function validURL(str) {
@@ -13,6 +26,7 @@ function validURL(str) {
       '(\\#[-a-z\\d_]*)?$',
     'i'
   );
+  return !!pattern.test(str);
 }
 
 function formSubmit(e) {
